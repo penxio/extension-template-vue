3618ee4c2047355e2d465a113e8023aea1634aa3
0xzion <0xzion.penx@gmail.com>
2024-06-01 23:56:03 +0800

    initial commit

---
+++ src/hello.command.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<template>
+  <div class="box">
+    <h1 class="text-purple-500 text-3xl">Hello Vue!</h1>
+  </div>
+</template>
+
+<style></style>
---
+++ src/index.command.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from 'vue'
+
+const count = ref(0)
+</script>
+
+<template>
+  <div class="box">
+    <h1 class="text-blue-500">Hello Vue!</h1>
+    <button type="button" @click="count++">count is {{ count }}</button>
+
+  </div>
+</template>
+
+<style></style>
